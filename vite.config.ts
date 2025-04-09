03aa4aad52c9463d879a862f5bb391c4789e251a
kgabeci <kgabeci@gmail.com>
2025-04-09 11:55:13 +0200

    Update Vercel configuration and Vite settings for improved build process

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,13 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "./",
   server: {
-    host: "::",
     port: 3000,
   },
   build: {
     outDir: "dist",
-    sourcemap: true,
+    sourcemap: process.env.NODE_ENV === "development",
     rollupOptions: {
       output: {
         manualChunks: {
